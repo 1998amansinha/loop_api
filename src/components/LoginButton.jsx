119b62546e1c7bf965a25bcc2287b75bf457e96f
[Aman Sinha] <1998amansinha@gmail.com>
2025-02-09 11:36:09 +0530

    Loading svg is added

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-const LoginButton = ({ label, type }) => {
+const LoginButton = ({ label, type, loading }) => {
   return (
     <button
       type={type}
-      className="p-4 mt-10 bg-white w-full mb-10 font-semibold text-black border border-solid border-black rounded-sm hover:bg-accent hover:text-white"
+      className="p-4 mt-10 bg-white w-full mb-10 font-semibold text-black border border-solid border-black rounded-sm hover:bg-accent hover:text-white flex items-center justify-center"
+      disabled={loading} // Disable button when loading
     >
-      {label}
+      {loading ? (
+        <img
+          src="https://res.cloudinary.com/do5xzpecm/image/upload/v1739077967/tube-spinner_dggcko.svg"
+          alt="Loading..."
+          className="w-6 h-6"
+        />
+      ) : (
+        label
+      )}
     </button>
   );
 };
