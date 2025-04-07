c4be53b3725657c1c51c2d39a7d6e5c69f7c623c
[Aman Sinha] <1998amansinha@gmail.com>
2025-04-07 09:45:59 +0530

    files updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/loop_api/", // 👈 this must match your repo name
   plugins: [react()],
-  base: "/loop-api",
 });
